0773cdf16d9e534a7bac124b5126bb4a938d9781
Ntando <yenkosindaba15@gmail.com>
2026-09-19 14:35:09 +0200

    created sale_items table in database

--- database.sql
+++ database.sql
@@ -1,6 +1,7 @@
-CREATE TABLE sales(
-    sale_id: INT PRIMARY KEY AUTO_INCREMENT,
-    sale_date: TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    total_amount: DECIMAL(10,2),
-    user_id: INT FOREIGN KEY REFERENCING users(user_id)
+CREATE TABLE sale_items(
+    sale_item_id: INT PRIMARY KEY AUTO_INCREMENT,
+    sale_id: INT FOREIGN KEY REFERENCING sales(sale_id),
+    medicine_id: INT FOREIGN KEY REFERENCING medicines(medicine_id),
+    quantity_sold: INT,
+    price_at_sale: DECIMAL(10,2)
 );
